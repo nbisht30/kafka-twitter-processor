945d1ce56cbaacc622a7e48fd739d234d2192acf
nbisht30 <nbisht30@gmail.com>
2019-11-21 10:11:07 +0530

    Added shutdown hook in producer

--- src/main/java/TwitterProducer.java
+++ src/main/java/TwitterProducer.java
@@ -25,6 +25,7 @@ public class TwitterProducer {
     String consumerSecret = "iOWDp8eSVceYic8b0uBk2DOoZLwmqMRgR5ZByxYBnqmbKdBYQg";
     String token = "960818204-1c8Sd4rB6JRDlw1mabaeEVt4GRh3qijRrKKFGWXe";
     String secret = "i3XKaPwNqGZgG4lZ1K6j6HnhfYJkecJ4L4vbH8NaZb5cd";
+    List<String> terms = Lists.newArrayList("kafka");
 
     public TwitterProducer() {
 
@@ -44,6 +45,15 @@ public class TwitterProducer {
 
         //create a Kafka producer
         KafkaProducer<String, String> producer = createKafkaProducer();
+
+        //add a shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Stopping application.\n Shutting down clients from Twitter");
+            client.stop();
+            logger.info("Closing producer.");
+            producer.close();
+            logger.info("Producer closed.");
+        }));
 
         //loop to send tweets to Kafka
         // on a different thread, or multiple different threads....
@@ -93,7 +103,7 @@ public class TwitterProducer {
         Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
         StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
         // Search terms for twitter:-
-        List<String> terms = Lists.newArrayList("bitcoin", "api");
+
         hosebirdEndpoint.trackTerms(terms);
 
         // These secrets should be read from a config file
